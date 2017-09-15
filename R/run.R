c472c7d730ceac72eb350db8c23212a25c8f6be1
Joe Wandy <joe.wandy@glasgow.ac.uk>
2017-09-15 16:44:01 +0100

    Add documentations

--- R/run.R
+++ R/run.R
@@ -1,5 +1,14 @@
-startShinyGradmatch <- function(x, ...)
-{
-  shiny::runApp(appDir = system.file("application", package='shinygradmatch'),
-                ...)
+#' Start ShinyGradmatch
+#' @title This function will start ShinyGradmatch
+#' @return Nothing
+#' @description An interactive Shiny application for running gradient matching to approximate ODEs.
+#' @details This starts the ShinyGradmatch application on the users local computer. 
+#' @keywords ShinyGradmatch
+#' @examples
+#' \dontrun{
+#' startShinyGradmatch()
+#' }
+#' @export
+startShinyGradmatch <- function() {
+  shiny::runApp(appDir = system.file("application", package='shinygradmatch'))
 }
---
+++ R/shinyGradmatch.R
@@ -0,0 +1,11 @@
+#' shinyGradmatch: A package for computating the notorious bar statistic.
+#'
+#' The shinyGradmatch package provides three categories of important functions:
+#' foo, bar and baz.
+#'
+#' @section Foo functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name shinyGradmatch
+NULL
